cb380148aed10e592086d5f7d945f37c64f5f6e2
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-06 10:08:42 +0530

    Update UrlLock.py

--- DaisyX/modules/UrlLock.py
+++ DaisyX/modules/UrlLock.py
@@ -10,31 +10,34 @@ from DaisyX.services.pyrogram import pbot
 @admins_only
 async def hmm(_, message):
     global daisy_chats
+    if not "can_change_info" in (await member_permissions(message.chat.id, message.from_user.id)):
+        await message.reply_text("**You don't have enough permissions**")
+        return
     if len(message.command) != 2:
         await message.reply_text(
             "I only recognize `/urllock on` and /urllock `off only`"
         )
-        message.continue_propagation()
+        return
     status = message.text.split(None, 1)[1]
     chat_id = message.chat.id
     if status == "ON" or status == "on" or status == "On":
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Link Block Already Activated In This Chat")
+            await lel.edit("URL Block Already Activated In This Chat")
             return
         await lel.edit(
-            f"Link Block Successfully Added For Users In The Chat {message.chat.id}"
+            f"URL Block Successfully Added For Users In The Chat {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("Link Block Was Not Activated In This Chat")
+            await lel.edit("URL Block Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"Link Block Successfully Deactivated For Users In The Chat {message.chat.id}"
+            f"URL Block Successfully Deactivated For Users In The Chat {message.chat.id}"
         )
     else:
         await message.reply_text(
@@ -48,17 +51,29 @@ async def hmm(_, message):
 async def hi(client,message):
   if not len(await member_permissions(message.chat.id, message.from_user.id)) < 1:
     message.continue_propagation()
+  if not len(await member_permissions(message.chat.id, BOT_ID)) < 1:
+    message.continue_propagation()
+  if not "can_delete_messages" in (await member_permissions(message.chat.id, BOT_ID)):
+    sedlyf = await message.reply_text("**I don't have enough permissions to delete messages here**")
+    await asyncio.sleep(10)                         
+    await sedlyf.delete()
   lel = get_url(message)
   if lel:
     try:
       await message.delete()
       sender = message.from_user.mention()
-      lol = await client.send_message(message.chat.id, f"{sender}, Your message will be deleted as it contain links. \n  Links are not allowed here")
+      lol = await client.send_message(message.chat.id, f"{sender}, Your message was deleted as it contain a link(s). \n ❗️ Links are not allowed here")
       await asyncio.sleep(10)                         
       await lol.delete()
     except Exception as e:
-      lol = await client.send_message(message.chat.id, "I can't delete messages here")
-      await asyncio.sleep(10)                         
-      await lol.delete()
+        message.continue_propagation()
   else:
     message.continue_propagation()
+
+__mod_name__ = "Link Block"
+__help__ = """
+<b> Block links sent by users in your group </b>
+
+- /lockurl [on/off]: Enable/Disable URL Lock
+
+"""
